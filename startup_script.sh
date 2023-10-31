258fe100c8c352009692f28020f44a13f694a98e
Will-Schr <will.a.schreiner@gmail.com>
2023-10-31 02:21:47 -0400

    apparently not

--- startup_script.sh
+++ startup_script.sh
@@ -4,7 +4,7 @@ eval "$(conda shell.bash hook)"
 
 # Set openapi key if it exists
 if [ "$OPENAI_APIKEY_INPUT" != "none" ]; then
-    export OPENAI_APIKEY=$(echo $OPENAI_APIKEY_INPUT);
+    export OPENAI_APIKEY=$(echo $OPENAI_APIKEY_INPUT)
 fi
 # Start server.py and jupyterlab
 cd /PI2
