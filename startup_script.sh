dc7f7494030aecbaca96f92c8cf58097cb2b4b79
Will-Schr <will.a.schreiner@gmail.com>
2023-10-31 01:03:26 -0400

    change to activate pi

--- startup_script.sh
+++ startup_script.sh
@@ -3,11 +3,11 @@
 eval "$(conda shell.bash hook)"
 
 # Set openapi key if it exists
-if [ $OPENAI_APIKEY_INPUT != "none" ]; then
+if [$OPENAI_APIKEY_INPUT != "none"]; then
     export OPENAI_APIKEY=$OPENAI_APIKEY_INPUT
 fi
 # Start server.py and jupyterlab
 cd /PI2
-conda run -n pi
+conda activate pi
 jupyter lab --allow-root --ip='*' --no-browser --NotebookApp.token='' --NotebookApp.password='' &
 python /PI2/pi-server/server.py &
