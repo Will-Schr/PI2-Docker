76776f122e7a2a4961fa416c530fbf838d9e44c2
Will-Schr <will.a.schreiner@gmail.com>
2023-10-31 12:45:57 -0400

    remove unneccesary section

--- startup_script.sh
+++ startup_script.sh
@@ -2,11 +2,6 @@
 
 eval "$(conda shell.bash hook)"
 
-# Set openapi key if it exists
-if [[ $OPENAI_APIKEY_INPUT != "none" ]] 
-then
-    export OPENAI_APIKEY=$(echo $OPENAI_APIKEY_INPUT)
-fi
 # Start server.py and jupyterlab
 cd /PI2
 conda activate pi
