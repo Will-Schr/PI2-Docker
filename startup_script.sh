333862f56153b60422d20ca8b9fc73a7ad4635bc
Will-Schr <will.a.schreiner@gmail.com>
2023-10-31 00:40:25 -0400

    fix hanging if

--- startup_script.sh
+++ startup_script.sh
@@ -7,7 +7,7 @@ conda run -n pi
 # Set openapi key if it exists
 if [$OPENAI_APIKEY_INPUT != "none"]; then
     export OPENAI_APIKEY=$OPENAI_APIKEY_INPUT
-
+fi
 # Start server.py and jupyterlab
 cd /PI2
 nohup jupyter lab --allow-root --ip='*' --no-browser --NotebookApp.token='' --NotebookApp.password='' & nohup python /PI2/pi-server/server.py &
